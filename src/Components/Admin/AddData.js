549583c9d294f453b0f643a53dcf3948b304160b
johannes <jmuljana@gmail.com>
2021-07-23 11:31:10 +1000

    adding columns in adddata

--- src/Components/Admin/AddData.js
+++ src/Components/Admin/AddData.js
@@ -6,10 +6,18 @@ export function AddData(props) {
       <input type="text" className="form-control" name="title" placeholder="Book title" id="title" />
       <label htmlFor="tagline">Tag Line</label>
       <input type="text" className="form-control" name="tagline" placeholder="Book tag line" id="tagline" />
-      <label htmlFor="isbn13">ISBN 13</label>
-      <input type="text" className="form-control" name="isbn13" placeholder="ISBN 13" id="isbn13" />
-      <label htmlFor="isbn10">ISBN 10</label>
-      <input type="text" className="form-control" name="isbn10" placeholder="ISBN 10" id="isbn10" />
+      
+      <div className="row">
+        <div className="col-md-6">
+          <label htmlFor="isbn13">ISBN 13</label>
+          <input type="text" className="form-control" name="isbn13" placeholder="ISBN 13" id="isbn13" />
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="isbn10">ISBN 10</label>
+          <input type="text" className="form-control" name="isbn10" placeholder="ISBN 10" id="isbn10" />
+        </div>
+      </div>
+      
       <label htmlFor="author">Author</label>
       <input type="text" className="form-control" name="author" placeholder="Book author" id="author" />
       <label htmlFor="publisher">Publisher</label>
@@ -20,7 +28,7 @@ export function AddData(props) {
       <input type="number" className="form-control" name="pages" placeholder="Pages" id="pages" />
       <label htmlFor="cover_image">Image</label>
       <input type="text" className="form-control" name="cover_image" placeholder="Cover image" id="cover_image" />
-      <div className="buttons row d-flex flex-row justify-content-between">
+      <div className="mt-3 buttons d-flex flex-row justify-content-between">
         <button type="reset" className="btn btn-secondary">Reset</button>
         <button type="submit" className="btn btn-primary">Add Book</button>
       </div>
