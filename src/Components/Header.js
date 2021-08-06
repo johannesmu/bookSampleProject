db7743d2a75bb0b3c86b0057ca447516427aadf3
johannes <jmuljana@gmail.com>
2021-08-06 21:23:27 +1000

    fixed header layout for mobile, added profile, fav

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,32 +1,42 @@
-import {NavLink} from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 export function Header(props) {
   const SiteNav = props.navigation.map((item, itemKey) => {
     return (
-      <li className="nav-item" key={itemKey}>
-        <NavLink exact to={item.link} className="nav-link" activeClassName="active">
-          {item.name}
-        </NavLink>
-      </li>
+      <NavLink key={itemKey} exact to={item.link} className="nav-link" activeClassName="active">
+        {item.name}
+      </NavLink>
     )
   })
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">{props.name}</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <Link className="navbar-brand order-0 order-sm-0" to="/">{props.name}</Link>
+        <button className="navbar-toggler order-3 order-sm-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <form className="d-flex flex-fill my-3 my-md-0 flex-fill order-4 order-sm-3">
+          <input className="form-control me-md-2" type="search" placeholder="Search for books" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </form>
+        <nav className="navbar navbar-nav flex-row mx-2 order-2 order-sm-2">
+          <Link className="nav-link px-3 py-sm-0 " to="/profile">
+            <span className="d-none d-sm-inline-block mx-2">Profile</span>
+            <i class="fas fa-user"></i>
+          </Link>
+          <Link className="nav-link px-3 py-sm-0" to="/favourites">
+            <span className="d-none d-sm-inline-block mx-2">Favourites</span>
+            <i class="fas fa-heart"></i>
+          </Link>
+        </nav>
+        <div className="collapse navbar-collapse order-5 order-sm-5" id="navbarSupportedContent">
+          <nav className="navbar-nav ms-auto mb-2 mb-lg-0">
             {SiteNav}
-          </ul>
-          <form className="d-flex">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
+          </nav>
         </div>
       </div>
-    </nav>
+    </div>
   )
 }
